790f4a86cf80fa17e8e704e10bddeb18dbefca16
Rick <r.aikema@ultraware.nl>
2017-05-05 12:33:35 +0200

    Add scoping option to AuthnRequest
    (cherry picked from commit 5a726dca119728b57a5aef3a41d6b010f5d87421)

--- src/Saml2/AuthnRequest.php
+++ src/Saml2/AuthnRequest.php
@@ -148,6 +148,26 @@ REQUESTEDAUTHN;
         $spEntityId = htmlspecialchars($spData['entityId'], ENT_QUOTES);
         $acsUrl = htmlspecialchars($spData['assertionConsumerService']['url'], ENT_QUOTES);
         $destination = $this->_settings->getIdPSSOUrl();
+        
+        $scoping = '';
+        if (isset($idpData['scoping'])) {
+            $proxyCount = (isset($idpData['scoping']['proxyCount']) ? ' ProxyCount="' . $idpData['scoping']['proxyCount'] . '"' : '');
+            $requesterId = (isset($idpData['scoping']['requesterId']) ? '<samlp:RequesterID>' . $idpData['scoping']['requesterId'] . '</samlp:RequesterID>' : '');
+            $idpList = '';
+            if (isset($idpData['scoping']['idpList'])) {
+                $idpList = '<samlp:IDPList>';
+                foreach ($idpData['scoping']['idpList'] as $idpListItem) {
+                    $idpList .= '<samlp:IDPEntry ProviderID="' . $idpListItem . '" />';
+                }
+                $idpList .= '</samlp:IDPList>';
+            }
+            $scoping = <<<SCOPING
+    <samlp:Scoping$proxyCount>
+       {$idpList}
+       {$requesterId}
+    </samlp:Scoping>
+SCOPING;
+        }
         $request = <<<AUTHNREQUEST
 <samlp:AuthnRequest
     xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
@@ -160,6 +180,7 @@ REQUESTEDAUTHN;
     ProtocolBinding="{$spData['assertionConsumerService']['binding']}"
     AssertionConsumerServiceURL="{$acsUrl}">
     <saml:Issuer>{$spEntityId}</saml:Issuer>{$subjectStr}{$nameIdPolicyStr}{$requestedAuthnStr}
+{$scoping}
 </samlp:AuthnRequest>
 AUTHNREQUEST;
 
